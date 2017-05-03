0ff6169f2f3229d08715701d49dd14038474eb9a
paulo <paulo@DESKTOP-8D4USIF>
2017-05-03 16:15:08 +0100

    4454

---
+++ gitTest/src/git/MyClass.java
@@ -0,0 +1,14 @@
+package git;
+
+public class MyClass {
+
+	public MyClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
